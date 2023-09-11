e2b263f4066814b4b4fdaa6259f9d620bcaa0b32
Zsea <yjhgef@163.com>
2023-09-11 18:36:38 +0000

    To complete the removal of payload protection.

--- src/packet/v1/parse.ts
+++ src/packet/v1/parse.ts
@@ -3,6 +3,8 @@ import { LongPacketType, IPacket, ILongPacket, InitialPacket, PacketType } from 
 import HKDF from "../../crypto/hkdf"
 import VariableInteger from "../../entities/VariableInteger";
 const SALT = Buffer.from("38762cf7f55934b34d179ae6a4c80cadccbb7f0a", "hex");
+const AEAD_ALGORITHM="aes-128-gcm",AEAD_ALGORITHM_AUTHTAG_LENGTH=16;
+const HEADER_ALGORITHM="aes-128-ecb";
 function ParseLongPacket(buffer: Buffer): ILongPacket {
     const type: LongPacketType = buffer[0] & 0x30;
     let offset = 5;
@@ -29,21 +31,32 @@ function ParseInitialPacket(buffer: Buffer, offset: number, dstIdLength: number,
     const iv = hkdf.qhkdfExpandLabel(client_initial_secret, "quic iv", 12);
     const hp = hkdf.qhkdfExpandLabel(client_initial_secret, "quic hp", 16);
     const sample = buffer.subarray(offset + 4, offset + 4+16);
-    const cipher = createCipheriv("aes-128-ecb", hp, Buffer.alloc(0));
+    const cipher = createCipheriv(HEADER_ALGORITHM, hp, Buffer.alloc(0));
     cipher.setAutoPadding(false);
     const updater = cipher.update(sample);
     const mask = updater.subarray(0, 5);
-    //let first=buffer[0]^(mask[0]&0xf);
-    //console.log('first',buffer[0],'===>',first&0x3)
-    const packetNumberLength=((buffer[0]^(mask[0]&0xf))&0x3)+1;
-    console.log('offset',offset,packetNumberLength)
+    buffer[0]=(buffer[0]^(mask[0]&0xf));
+    const packetNumberLength=(buffer[0]&0x3)+1;
     const pn=buffer.subarray(offset,offset+packetNumberLength);offset+=packetNumberLength;
     for(let i=0;i<pn.length;i++){
-        pn[i]=pn[i]^mask[i+1]
+        pn[i]=pn[i]^mask[i+1];
     }
-    console.log('pn',pn);
+    const aad=buffer.subarray(0,offset);
     const payload=buffer.subarray(offset);
-    console.log(payload)
+    //console.log(payload);
+    const nonce=Buffer.alloc(iv.length);
+    for(let i=iv.length-1,pi=pn.length-1;i>=0;i--,pi--){
+        let n=0;
+        if(pi>=0) n=pn[pi];
+        nonce[i]=n^iv[i];
+    }
+    const authTag=payload.subarray(payload.length-AEAD_ALGORITHM_AUTHTAG_LENGTH);
+    const encPayload=payload.subarray(0,payload.length-AEAD_ALGORITHM_AUTHTAG_LENGTH);
+    const decipher=createDecipheriv(AEAD_ALGORITHM,key,nonce);
+    decipher.setAAD(aad);
+    decipher.setAuthTag(authTag);
+    const plain=Buffer.concat([decipher.update(encPayload),decipher.final()]);
+    console.log('明文',plain.length,plain.toString("hex"));
     return new InitialPacket();
 }
 export {
